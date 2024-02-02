30abc3b726ca1c9bb3e1afcb2beed77c9abd2e19
shkholikov <imshakhzod@gmail.com>
2024-02-02 23:03:16 +0100

    Main page wording change.

--- app/page.tsx
+++ app/page.tsx
@@ -15,11 +15,8 @@ export default function Page() {
           <p
             className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}
           >
-            <strong>Welcome to Acme.</strong> This is the example for the{' '}
-            <a href="https://nextjs.org/learn/" className="text-blue-500">
-              Next.js Learn Course
-            </a>
-            , brought to you by Vercel.
+            <strong>Welcome to Acme.</strong> This is your invoice management
+            tool.
           </p>
           <Link
             href="/login"
