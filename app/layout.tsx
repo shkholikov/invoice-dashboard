7472c35038604b5a7c421c1fec5f968025edc298
shkholikov <imshakhzod@gmail.com>
2024-02-04 01:41:26 +0100

    Add metadata and change pages titles.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,15 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Invoice Dashboard',
+    default: 'Invoice Dashboard',
+  },
+  description: 'Invoice Management Tool Build in Next.js',
+  metadataBase: new URL('https://invoice-dashboard-nine.vercel.app'),
+};
 
 export default function RootLayout({
   children,
